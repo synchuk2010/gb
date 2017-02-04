55f29a2cd6df2c4735209af201abce16897ec8c8
Никита Фисун <nikita_c47@outlook.com>
2017-02-04 11:37:12 +0500

    Коммит изменений для слияния

---
+++ migrations/m170204_063632_create_initial_structure.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m170204_063632_create_initial_structure extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170204_063632_create_initial_structure cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
